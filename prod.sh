3261fb1d452b2e7a050fa912b96aec2d5ffbb095
Selby Kendrick <selby.kendrick@gmail.com>
2015-10-31 23:20:43 -0500

    ok lets try agin

---
+++ prod.sh
@@ -0,0 +1,12 @@
+# Mysql Connection for user's identifying info
+export ETP_DB_USER=etp
+export ETP_DB_PASS=etp
+export ETP_DB_HOST=localhost
+export ETP_DB_PORT=3306
+
+export CLOUDDB_USER=93536182-8a0c-4cbb-b2b9-e7348ca02c15-bluemix
+export CLOUDDB_PASS=f25b52e8c0a76e2fd5c5fcfcc26d0727469f3e86e47e7da3cb17a1d2f3f8ab64
+export CLOUDDB_HOST=93536182-8a0c-4cbb-b2b9-e7348ca02c15-bluemix.cloudant.com
+export CLOUDDB_PORT=443
+
+node app.js --prod
